d32aec5684825708f86395b69686edefa399bd41
chongshen2000 <chongshen2000@hotmailcom>
2021-05-31 15:36:56 +0800

    hehe

--- payment.php
+++ payment.php
@@ -4,7 +4,20 @@ if (empty($_SESSION['user_email'])) {
   echo "<script>window.open('./index.php','_self')</script>";
 }
 include("functions/functions.php");
+include("includes/db.php");
 
+
+$user = $_SESSION['user_email'];
+$get_user = "select * from user where user_email='$user'";
+$run_user = mysqli_query($con, $get_user);
+$row_user = mysqli_fetch_array($run_user);
+
+$u_id = $row_user['user_id'];
+$name = $row_user['user_name'];
+$email = $row_user['user_email'];
+$address = $row_user['user_address'];
+$state = $row_user['user_state'];
+$contact = $row_user['user_contact'];
 ?>
 
 <!DOCTYPE html>
@@ -30,25 +43,33 @@ include("functions/functions.php");
           <div class="row">
             <div class="col-50">
               <h3>Billing Address</h3>
-              <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-              <input type="text" id="fname" name="firstname">
-              <label for="email"><i class="fa fa-envelope"></i> Email</label>
-              <input type="text" id="email" name="email">
-              <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-              <input type="text" id="adr" name="address">
-              <label for="city"><i class="fa fa-institution"></i> City</label>
-              <input type="text" id="city" name="city">
+              <label for="u_name"><i class="fa fa-user"></i> Full Name</label>
+              <input type="text" name="u_name" value="<?php echo $name; ?>" pattern="[A-Za-z\s.,&]{0,25}" required />
+              <label for="u_email"><i class="fa fa-envelope"></i> Email</label>
+              <input type="email" name="u_email" size="40" value="<?php echo $email; ?>" required />
+              <label for="u_address"><i class="fa fa-address-card-o"></i> Address</label>
+              <input type="text" name="u_address" value="<?php echo $address; ?>" required>
+              <label for="u_state"><i class="fa fa-institution"></i> State</label>
+              <select name="u_state">
+                <option><?php echo $state; ?></option>
+                <option>Perlis</option>
+                <option>Kedah</option>
+                <option>Selangor</option>
+                <option>Kuala Lumpur</option>
+                <option>Johor</option>
+                <option>Pahang</option>
+                <option>Perak</option>
+                <option>Penang</option>
+                <option>Negeri Sembilan</option>
+                <option>Malacca</option>
+                <option>Terengganu</option>
+                <option>Kelantan</option>
+                <option>Sabah</option>
+                <option>Sarawak</option>
 
-              <div class="row">
-                <div class="col-50">
-                  <label for="state">State</label>
-                  <input type="text" id="state" name="state">
-                </div>
-                <div class="col-50">
-                  <label for="zip">Zip</label>
-                  <input type="text" id="zip" name="zip">
-                </div>
-              </div>
+              </select>
+              <label for="u_contact"><i class="fa fa-address-card-o"></i>Contact Number</label>
+              <input type="text" name="u_contact" placeholder="Eg:0122345678" pattern="[0]{1}[1]{1}[0-9]{8,9}" value="<?php echo $contact; ?>" required />
             </div>
 
             <div class="col-50">
@@ -83,10 +104,66 @@ include("functions/functions.php");
           <label>
             <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
           </label>
-          <input type="submit" value="Continue to checkout" class="btn">
+          <input type="submit" name="orders" value="Confirm Order" />
         </form>
       </div>
     </div>
+
+    <?php
+    if (isset($_POST['orders'])) {
+
+      //getting the text data from the fields
+      $total = 0;
+      $tt = date('Y-m-d');
+      $reference = (rand(1000, 99999));
+
+      $user_id = $u_id;
+      $user_name = $_POST['u_name'];
+      $user_email = $_POST['u_email'];
+      $user_address = $_POST['u_address'];
+      $user_contact = $_POST['u_contact'];
+
+      $sel_price = "select * from cart";
+      $run_price = mysqli_query($con, $sel_price);
+
+      while ($p_price = mysqli_fetch_array($run_price)) {
+
+        $pro_id = $p_price['product_id']; //from cart table
+        $pro_qty = $p_price['qty']; // from cart table
+
+        //declare data from products table
+        $pro_price = "select * from products where sport_id = '$pro_id'";
+        $run_pro_price = mysqli_query($con, $pro_price);
+
+        while ($pp_price = mysqli_fetch_array($run_pro_price)) {
+
+          $sport_price = array($pp_price['sport_price']); //put price into $sport_price
+          $single_price = $pp_price['sport_price'];
+          $sports_price = $single_price * $pro_qty;
+          $values = array_sum($sport_price);
+          $total += $sports_price;
+          $discount_price = $total * .10;
+          $promotion_price = $total - $discount_price;
+
+
+
+
+
+          $insert_detail = "select user_id from orders where user_id = '$user_id'";
+          $run_detail = mysqli_query($con, $insert_detail);
+
+          $delivery_details = "insert into orders (user_id,customer_name,customer_email,customer_address,customer_contact,date,order_reference,total_price) values
+        ('$user_id','$user_name','$user_email','$user_address','$user_contact','$tt','$reference','$promotion_price')";
+          $insert_delivery = mysqli_query($con, $delivery_details);
+          if ($insert_delivery) {
+
+            include("delivery_details.php");
+          }
+
+    ?>
+    <?php }
+      }
+    } ?>
 
     <div class="col-25">
       <div class="container">
