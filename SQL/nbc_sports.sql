63cd01f882e81736c6d3069a1493984a064caad2
Chris Wong <chriswongez@gmail.com>
2021-06-06 18:27:18 +0800

    c

--- SQL/nbc_sports.sql
+++ SQL/nbc_sports.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 04, 2021 at 06:36 PM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Generation Time: Jun 06, 2021 at 12:26 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -108,7 +107,9 @@ INSERT INTO `orders` (`order_id`, `user_id`, `customer_name`, `customer_email`, 
 (11, 3, 'Grace Tan', 'grace@gmail.com', '1,JLN Ahmad, 84000 Muar, Johor', 173066509, '2017-12-27', 64578, '89.00'),
 (12, 1, 'Mika', 'mika@gmail.com', '123, Mika Street', 101111111, '2016-12-27', 7468, '987.00'),
 (13, 1, 'Mika', 'mika@gmail.com', '123, Mika Street', 101111111, '2021-04-23', 45198, '8910.00'),
-(14, 5, 'Alice Thum', 'alice@gmail.com', '123, Alice Street', 101111115, '2021-06-02', 76900, '314.10');
+(14, 5, 'Alice Thum', 'alice@gmail.com', '123, Alice Street', 101111115, '2021-06-02', 76900, '314.10'),
+(15, 7, 'Chris Wong', 'chriswongez@gmail.com', '32, Jalan SJI 5,Taman Seremban Jaya Idaman', 1111142344, '2021-06-06', 82819, '80.10'),
+(16, 7, 'Chris Wong', 'chriswongez@gmail.com', '32, Jalan SJI 5,Taman Seremban Jaya Idaman', 1111142344, '2021-06-06', 30838, '539.10');
 
 -- --------------------------------------------------------
 
@@ -143,7 +144,12 @@ INSERT INTO `ordersdetail` (`orderdetail_ID`, `pro_id`, `pro_title`, `price`, `q
 (10, 7, 'Under Armour Hoodie Sportswear', '349.00', 1, 5, 'done'),
 (11, 2, 'Adidas T-shirt price', '178.00', 2, 6, 'done'),
 (28, 9, 'Under Armour Hoodie ', '349.00', 1, 14, 'process'),
-(27, 2, 'Nike Men Air Max 720 T-Shirt', '9900.00', 100, 13, 'process');
+(27, 2, 'Nike Men Air Max 720 T-Shirt', '9900.00', 100, 13, 'process'),
+(29, 5, 'Adidas T-Shirt', '89.00', 1, 14, 'process'),
+(30, 2, 'Nike Men Air Max 720 T-Shirt', '99.00', 1, 14, 'process'),
+(31, 7, 'Under Armour Curry 8', '599.00', 1, 16, 'process'),
+(32, 4, 'Adidas Sportswear Hoodie', '229.00', 1, 16, 'process'),
+(33, 3, 'Nike Sportswear Hoodie', '199.99', 1, 16, 'process');
 
 -- --------------------------------------------------------
 
@@ -157,23 +163,24 @@ CREATE TABLE `products` (
   `description` text NOT NULL,
   `sport_category` varchar(100) NOT NULL,
   `sport_price` decimal(9,2) NOT NULL,
-  `sport_image` text NOT NULL
+  `sport_image` text NOT NULL,
+  `isHidden` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`sport_id`, `sport_name`, `description`, `sport_category`, `sport_price`, `sport_image`) VALUES
-(1, 'Nike Air Force 1', 'Nike Shoes', '1', '699.00', 'Nike Shoes.jpeg'),
-(2, 'Nike Men Air Max 720 T-Shirt', 'Nike T-Shirt', '1', '99.00', 'Nike T-Shirt.jpeg'),
-(3, 'Nike Sportswear Hoodie', 'Nike Hoodie', '1', '199.99', 'Nike Hoodie.jpeg'),
-(4, 'Adidas Sportswear Hoodie', 'Adidas Hoodie', '2', '229.00', 'Adidas Hoodie.jpeg'),
-(5, 'Adidas T-Shirt', 'Adidas Men T-Shirt', '2', '89.00', 'Adidas T-Shirt.jpeg'),
-(6, 'Adidas Ultraboost 4.0 DNA ', 'Adidas Shoes', '2', '799.00', 'Adidas Shoes.jpeg'),
-(7, 'Under Armour Curry 8', 'Under Armour Shoes', '3', '599.00', 'Under Armour Shoes.jpeg'),
-(8, 'Under Armour UA Tech', 'Under Armour T-Shirt', '3', '120.00', 'Under Armour T-Shirt.jpeg'),
-(9, 'Under Armour Hoodie ', 'Under Armour Hoodie', '3', '349.00', 'Under Armour Hoodie.jpeg');
+INSERT INTO `products` (`sport_id`, `sport_name`, `description`, `sport_category`, `sport_price`, `sport_image`, `isHidden`) VALUES
+(1, 'Nike Air Force 1', 'Nike Shoes', '1', '699.00', 'Nike Shoes.jpeg', 0),
+(2, 'Nike Men Air Max 720 T-Shirt', 'Nike T-Shirt', '1', '99.00', 'Nike T-Shirt.jpeg', 0),
+(3, 'Nike Sportswear Hoodie', 'Nike Hoodie', '1', '199.99', 'Nike Hoodie.jpeg', 0),
+(4, 'Adidas Sportswear Hoodie', 'Adidas Hoodie', '2', '229.00', 'Adidas Hoodie.jpeg', 0),
+(5, 'Adidas T-Shirt', 'Adidas Men T-Shirt', '2', '89.00', 'Adidas T-Shirt.jpeg', 0),
+(6, 'Adidas Ultraboost 4.0 DNA ', 'Adidas Shoes', '2', '799.00', 'Adidas Shoes.jpeg', 0),
+(7, 'Under Armour Curry 8', 'Under Armour Shoes', '3', '599.00', 'Under Armour Shoes.jpeg', 0),
+(8, 'Under Armour UA Tech', 'Under Armour T-Shirt', '3', '120.00', 'Under Armour T-Shirt.jpeg', 0),
+(9, 'Under Armour Hoodie ', 'Under Armour Hoodie', '3', '349.00', 'Under Armour Hoodie.jpeg', 0);
 
 -- --------------------------------------------------------
 
@@ -203,29 +210,12 @@ INSERT INTO `user` (`user_id`, `user_name`, `user_email`, `user_pass`, `user_pas
 (3, 'Grace Tan', 'grace@gmail.com', 'huihui12345678901', 'huihui1102', '1,JLN Ahmad, 84000 Muar, Johor.', 'Johor', '0173066509', '037e79b2fb52127537be79110891ae3f--smiley-emoji-emoji-faces.jpg'),
 (4, 'Tom', 'tom@gmail.com', 'tom12345678901', 'tom123', '123, Tom Street', 'Sabah', '0101111114', '957c3f835d2a915ef8020a9a61aa84e2.jpg'),
 (5, 'Alice Thum', 'alice@gmail.com', 'alice12345678901', 'alice123', '123, Alice Street', 'Penang', '0101111115', '6533518-1x1-340x340.jpg'),
-(6, 'Candy', 'candy@gmail.com', 'candy12345678901', 'candy123', '123, Candy Street', 'Penang', '0123342454', 'images.jpg');
+(6, 'Candy', 'candy@gmail.com', 'candy12345678901', 'candy123', '123, Candy Street', 'Penang', '0123342454', 'images.jpg'),
+(7, 'Chris Wong', 'chriswongez@gmail.com', '111112222222222', '111112222222222', '32, Jalan SJI 5,\r\nTaman Seremban Jaya Idaman', 'Negeri Sembilan', '01111142344', 'ezgif.com-gif-maker (5).gif');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`admin_id`);
-
---
--- Indexes for table `cart`
---
-ALTER TABLE `cart`
-  ADD PRIMARY KEY (`product_id`);
-
---
--- Indexes for table `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`cat_id`);
 
 --
 -- Indexes for table `orders`
@@ -243,8 +233,7 @@ ALTER TABLE `ordersdetail`
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`sport_id`),
-  ADD KEY `sport_category` (`sport_category`);
+  ADD PRIMARY KEY (`sport_id`);
 
 --
 -- Indexes for table `user`
@@ -257,40 +246,28 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `admin_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `category`
---
-ALTER TABLE `category`
-  MODIFY `cat_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `order_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `ordersdetail`
 --
 ALTER TABLE `ordersdetail`
-  MODIFY `orderdetail_ID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `orderdetail_ID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `sport_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `sport_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
